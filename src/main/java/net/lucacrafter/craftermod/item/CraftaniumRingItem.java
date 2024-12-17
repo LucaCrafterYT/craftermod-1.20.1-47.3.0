78ec59ac8a247d02b487102ccbf4349aa8a5f076
LucaCrafterYT <cutiepinkypaws@yahoo.com>
2024-12-17 16:42:25 +0100

    update #5

--- src/main/java/net/lucacrafter/craftermod/item/CraftaniumRingItem.java
+++ src/main/java/net/lucacrafter/craftermod/item/CraftaniumRingItem.java
@@ -2,24 +2,22 @@
 package net.lucacrafter.craftermod.item;
 
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.network.chat.Component;
 
-import net.lucacrafter.craftermod.procedures.CraftaniumRingRightClickProcedure;
+import java.util.List;
 
 public class CraftaniumRingItem extends Item {
 	public CraftaniumRingItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.COMMON));
 	}
 
 	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		CraftaniumRingRightClickProcedure.execute(entity);
-		return ar;
+	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, level, list, flag);
+		list.add(Component.literal("Just a Ring"));
 	}
 }
